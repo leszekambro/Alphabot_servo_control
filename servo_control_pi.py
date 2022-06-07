d6191ce40ee573587bd22ffe1fd21377d02535d0
leszekambro <58566617+leszekambro@users.noreply.github.com>
2022-06-07 22:12:52 +0200

    Add files via upload

---
+++ servo_control_pi.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 22, PWM 50Hz
+p.start(0) # Inicjalizacja
+try:
+  p.ChangeDutyCycle(5) #duty 2-12 dla kątów 0-180
+  time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
